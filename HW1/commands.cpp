796c88267c880bdef6c7e79b770f07c6c07e602d
OS 046209 <itai208@gmail.com>
2022-11-14 15:15:27 +0200

    finished fg&bg

--- HW1/commands.cpp
+++ HW1/commands.cpp
@@ -10,7 +10,7 @@ using namespace std;
 // Parameters: pointer to jobs, command string
 // Returns: 0 - success,1 - failure
 //**************************************************************************************
-int ExeCmd(void* jobs, char* lineSize, char* cmdString){
+int ExeCmd(list<job>* jobs, char* lineSize, char* cmdString){
 	char* cmd; 
 	char* args[MAX_ARG];
 	char pwd[MAX_LINE_SIZE];
@@ -37,6 +37,7 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString){
 		
 	} 
 	
+/*************************************************/
 
 	else if (!strcmp(cmd, "pwd") || !strcmp(cmd, "&pwd") ){
 	
@@ -46,14 +47,18 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString){
         return SUCCESS	
 	}
 	
+/*************************************************/
 
 	else if (!strcmp(cmd, "mkdir") || !strcmp(cmd, "&mkdir") ){
 	
  		
 	}
 
-	
+/*************************************************/	
+
 	else if (!strcmp(cmd, "jobs") || !strcmp(cmd, "&jobs") ){
+		if(jobs == null)
+			return SUCCESS;
         /*note: job has the members:
             job_id
             command
@@ -75,21 +80,126 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString){
         }	
 	}
 
+/*************************************************/
+
 	else if (!strcmp(cmd, "showpid") || !strcmp(cmd, "&showpid") ){
-	
+		
 		
 	}
 
+/*************************************************/
+
+	// fix :
+	//	1.what to do with fg procces
+	//  2.error invaild args
 	else if (!strcmp(cmd, "fg") || !strcmp(cmd, "&fg") ) {
-	
-		
-	} 
-	
-	else if (!strcmp(cmd, "bg") || !strcmp(cmd, "&bg") ){
-	
-  		
+		if(jobs == null){
+			cout << "smash error: fg: jobs list is empty" << endl;
+			return FAILURE	
+		}
+
+		if(num_arg >1 ){
+			cout << "smash error: fg: invaild arguments" << endl;
+			return FAILURE
+		}
+
+		list<job>::iterator list_it = jobs->begin();
+
+		//given job has no id
+		if(num_arg == 0){
+			list_it= jobs->pop_back();
+			cout << list_it->command ":" list_it->job_id << endl;
+			list_it->state = FORGROUND_STATE;
+			kill(list_it->job_id,SIGCONT);
+			waitpid(list_it->job_id);
+			return SUCCESS;
+		}
+
+		//given job has id
+		given_id = atoi(arg[1]);
+		while(list_it != jobs->end()){
+			if(list_it->job_id = given_id){
+				cout << list_it->command ":" list_it->job_id << endl;
+				jobs->erase(list_it);
+				list_it->state = FORGROUND_STATE;
+				kill(list_it->job_id,SIGCONT);
+				waitpid(list_it->job_id);
+				return SUCCESS;
+			}
+			list_it++;
+		}
+		//no matched id in jobs list
+		cout << "smash error: fg: job-id "given_id" does not exist" << endl;
+		return FAILURE;
 	}
+
+/*************************************************/	
 	
+	// fix :
+	//	1.what to do with fg procces
+	//  2.error invaild args
+	else if (!strcmp(cmd, "bg") || !strcmp(cmd, "&bg") ){
+		if(jobs == null){
+			cout << "smash error: bg: job-id "arg[1]" does now exist" << endl;
+			return FAILURE	
+		}
+
+		if(num_arg > 1){
+			"smash error: bg: invalid arguments"<< endl;
+			return FAILURE
+		}
+
+		list<job>::iterator list_it = jobs->begin();
+		list<job>::iterator max_id_stopped_job = null;
+
+		//given job has no id
+		if(num_arg == 0){
+			while(list_it != jobs->end()){
+				if(list_it->state == STOP_STATE)
+					max_id_stopped_job = list_it;
+				list_it++;
+			}
+			
+			//no job is in stop state 
+			if(max_id_stopped_job == null){
+				cout << "smash error: bg: there are no stopped jobs to resume"
+				return FAILURE;	
+			}
+
+			cout << max_id_stopped_job->command ":" max_id_stopped_job->job_id << endl;
+			max_id_stopped_job->state = BACKGROUND_STATE;
+			kill(max_id_stopped_job->job_id,SIGCONT); 
+			return SUCCESS;
+		}
+
+		//given job has id
+		given_id = atoi(arg[1]);
+		while(list_it != jobs->end()){
+				if(list_it->job_id == given_id){
+					if(list_it->state == STOP_STATE){
+						cout << list_it->command ":" list_it->job_id << endl;
+						list_it->state = BACKGROUND_STATE;
+						kill(max_id_stopped_job->job_id,SIGCONT); 
+						return SUCCESS;
+					}
+
+					//given job is in BACKGROUND STATE
+					else{
+						cout << "smash error: bg: job-id "list_it->job_id" is already running in the background" << endl;
+						return FAILURE
+
+					}
+				}
+				it++;
+			}
+
+			//didnt find the given job
+			cout << "smash error: bg: job-id "given_id" does not exist" << endl;
+			return FAILURE;
+	}
+			
+/*************************************************/
+
 	else if (!strcmp(cmd, "quit") || !strcmp(cmd, "&quit") ){
 	
    		
