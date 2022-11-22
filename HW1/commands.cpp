7178fbc86a63a95bf4bac04e5572625273d9cfa1
OS 046209 <itai208@gmail.com>
2022-11-23 00:07:05 +0200

    gdfg

--- HW1/commands.cpp
+++ HW1/commands.cpp
@@ -507,7 +507,7 @@ int BgCmd(char* lineSize){
 		//not built in cmd && bg cmd
 		if(!is_built_in_cmd(command)){
 			ExeExternal(args, command, true, full_command);
-			return 2;
+			return 0;
 		}
 		else{
 			return -1;
